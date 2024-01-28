acd295b5a3af0981b6d4b1b0fff4672a2f80eee4
Naruxtian <Naruxtian@Hotmail.com>
2024-01-28 16:51:16 -0600

    fix

--- functions/investigate.py
+++ functions/investigate.py
@@ -3,5 +3,6 @@ import wikipedia
 def investigate(rec, talk):
     search = rec.replace('investiga', '')
     talk('Investigando'+ search)
+    wikipedia.set_lang('es')
     info = wikipedia.summary(search, 1)
     talk(info)
